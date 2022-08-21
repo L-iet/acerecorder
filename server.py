5e4e75d63b7f0ccc21cb0d59c71cc440b30fb466
Joshua <joshjohnmark@gmail.com>
2022-08-21 22:02:39 +0100

    fixed cors issue

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import request, redirect, jsonify, Flask
+from flask_cors import CORS
 import requests
 import json
 import os
@@ -6,24 +7,26 @@ import os
 port = 7777
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 def upload():
-	text_rec_s = request.json['textRec']
-	text_rec = json.loads(text_rec_s)
+	text_rec = request.json['textRec']
+	# text_rec = json.loads(text_rec_s)
 	if 'auxmedia' in text_rec[0]:
 		# do stuff to get the media
 		link = request.json['link']
 		pass
-	with open('textfiles/' + request.json['filename'] + '.trf', 'w') as f:
-		f.write(text_rec_s)
+	with open(f'textfiles/{ request.json["filename"] }.trf', 'w') as f:
+		json.dump(text_rec, f)
 	print(os.listdir('textfiles'))
+	return "Done."
 
 
 
 
 if __name__ == '__main__':
-	app.run(port=port)
+	app.run(port=port,debug=True)
 
 
 
