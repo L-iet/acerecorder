f994a634a8a7cd4d3b78e17784ec750e6d33d9ad
Joshua <joshjohnmark@gmail.com>
2022-08-22 05:17:16 +0100

    rewrote into two endpoints

--- server.py
+++ server.py
@@ -11,31 +11,40 @@ from io import FileIO
 test = False
 port = 7777 if test else os.environ["PORT"]
 
+_origins = {"origins":"https://l-iet.github.io"}
+
 app = Flask(__name__)
 # app.config['CORS_HEADERS'] = 'Content-Type'
-CORS(app, resources={r"/": {"origins": "https://l-iet.github.io"}})
+CORS(app, resources={r"/": _origins, r"/media":_origins})
 
 @app.route('/', methods=["POST"])
 @cross_origin(origin="https://l-iet.github.io")
-def upload():
-	print('received post')
+def uploadText():
+	print('received text')
 	text_rec = request.json['textRec']
 	print(text_rec[0])
-	if 'auxmedia' in text_rec[0]:
-		# do stuff to get the media
-		print(request.json['media'])
-		with open('media.webm', 'wb+') as media:
-			media.write(request.json['media'])
-			media.seek(0)
-			tvm_url = googcloud.upload(f"{request.json['filename']}.webm",file_obj=media)
-	else:
-		tvm_url = None
+	
 	tvf_url = googcloud.upload(f"{request.json['filename']}.tvf",file_str=json.dumps(text_rec))
 
-	resp = make_response(jsonify([tvf_url, tvm_url]), 200)
+	resp = make_response(tvf_url, 200)
 	resp.headers["Content-Type"] = "application/json"
 	resp.headers["Access-Control-Allow-Origin"] = "https://l-iet.github.io"
 	return resp
+
+@app.route('/media', methods=["POST"])
+@cross_origin(origins="https://l-iet.github.io")
+def uploadMedia():
+	print('receiving media')
+	print(request.data)
+	with open('media.webm', 'wb+') as media: # might consider creating distinct file in case of simultaneous
+		media.write(request.data)
+		media.seek(0)
+		tvm_url = googcloud.upload(f"{request.json['filename']}.webm",file_obj=media)
+	resp = make_response(tvm_url, 200)
+	resp.headers["Content-Type"] = "application/json"
+	resp.headers["Access-Control-Allow-Origin"] = "https://l-iet.github.io"
+	return resp
+
 
 
 
